262ae86ae4a3a97b9b0d1c099d9965185a3f8bd7
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-06-25 14:34:42 -0300

    Inheritance-and-polymorphism

--- Program.cs
+++ Program.cs
@@ -11,13 +11,11 @@ namespace Course
             BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
             // UPCASTING
-
             Account acc1 = bacc;
             Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
             Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
 
             // DOWNCASTING
-
             BusinessAccount acc4 = (BusinessAccount)acc2;
             acc4.Loan(100.0);
 
